89ba6e5ba28375605691184174ec9ade038320ad
myname0 <eliseevalizaveta@gmail.com>
2018-03-21 19:25:00 +0400

    2

--- src/main/java/ssu/tasks/Task5.java
+++ src/main/java/ssu/tasks/Task5.java
@@ -1,18 +1,34 @@
 package ssu.tasks;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Task5 {
     public void execute() {
         Random random = new Random();
         double constant = -1.00 / 15.00;
-        double X = 0.00;
+        double X;
         double sum = 0.00;
+        List<Double> listX;
+        double manTime;
         for (int i = 0; i < 1000; i++) {
-            X = (X + constant * Math.log(random.nextDouble()));
-            sum += X;
-
+            X = 0.0;
+            listX = new ArrayList<Double>();
+            for(int j = 0; j < 96; j++) {
+                X = X + constant * Math.log(random.nextDouble());
+                listX.add(X);
+            }
+            manTime = random.nextDouble() * listX.get(listX.size() - 1);
+            sum += findClosest(listX, manTime);
         }
         System.out.println(sum / 1000.00);
+    }
+
+    private double findClosest(List<Double> list, double x){
+        for (Double number : list) {
+            if(number >= x) return number - x;
+        }
+        return 0;
     }
 }
